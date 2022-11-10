6ce69fd3840e6fa4f14317200a674ec50b752fb7
Lebedev Danila <lebedevweb@mail.ru>
2022-11-11 02:00:32 +0300

    add styles

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ gulp.task('browser-sync', function() {
 
 gulp.task('default', gulp.series('browser-sync'));
 
-gulp.watch('./resources/sass/modules/**/*.scss')
+gulp.watch('./resources/sass/**/*.scss')
     .on('change', function(path, stats) {
         setTimeout(function(){
             return browserSync.reload();
